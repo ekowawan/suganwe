69414a3165b76b9b7eaefa4cc6a542981fb3cba1
ekowawan <31313477+ekowawan@users.noreply.github.com>
2017-11-02 22:04:37 +0700

    Update pcbpost.js

--- pcbpost.js
+++ pcbpost.js
@@ -21,11 +21,11 @@ if(a_to_vh==false)
 {var encryptedUrl=Base64.encode(a_to_vi);
 var x=Math.floor((Math.random()*5)+ 1);
 var xxx=null;
-if(y=="1"){yyy="/2017/08/pcb-design-tutorial-how-to-layout-with.html"}
-if(y=="2"){yyy="/2017/11/how-to-choose-professional-pcb.html"}
-if(y=="3"){yyy="/2017/11/advantages-of-using-consultancy-for.html"}
-if(y=="4"){yyy="/2017/11/manufacturing-process-of-printed.html"}
-if(y=="5"){yyy="/2017/11/guidelines-best-practices-in-flexible.html"}a_to_ve[i].href="http://www.pcbboard.review"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="1"){xxx="/2017/08/pcb-design-tutorial-how-to-layout-with.html"}
+if(x=="2"){xxx="/2017/11/how-to-choose-professional-pcb.html"}
+if(x=="3"){xxx="/2017/11/advantages-of-using-consultancy-for.html"}
+if(x=="4"){xxx="/2017/11/manufacturing-process-of-printed.html"}
+if(x=="5"){xxx="/2017/11/guidelines-best-practices-in-flexible.html"}a_to_ve[i].href="http://www.pcbboard.review"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
